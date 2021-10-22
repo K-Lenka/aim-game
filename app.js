a96daaae1058dfb54af2b5b0d05bea2b6c0f3237
Elena <alena1027@gmail.com>
2021-10-21 21:45:00 -0300

    feat: declare variables in js

---
+++ app.js
@@ -0,0 +1,15 @@
+const startBtn = document.querySelector("#start");
+const screens = document.querySelectorAll(".screen");
+const timeList = document.querySelector("#time-list");
+const timeEl = document.querySelector("#time");
+const board = document.querySelector("#board");
+const colors = [
+  "#8ecae6",
+  "#219ebc",
+  "#023047",
+  "#ffb703",
+  "#fb8500",
+  "#f9844a",
+  "#f94144",
+  "#90be6d",
+];
